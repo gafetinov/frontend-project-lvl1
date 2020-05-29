8875a9e0c4bd503f05ca63eb93e9aeb486f41bb4
gafetinov <artem.gafetinov@gmail.com>
2020-05-29 21:24:41 +0500

    Refactor

--- src/cli.js
+++ src/cli.js
@@ -1,35 +1,55 @@
 import readlineSync from 'readline-sync';
 import random from 'lodash/random.js';
 
+let USER_NAME;
 
 const isEven = (number) => number % 2 === 0;
 
+const tellRule = (rule) => {
+  console.log(rule);
+};
+
+const verdict = (correctAnswer, userAnswer) => {
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+    console.log(`Let's try again, ${USER_NAME}!`);
+    process.exit();
+  }
+};
+
+const getAnswerToQuestion = (question) => {
+  console.log(`Question: ${question}`);
+  return readlineSync.question('Your answer: ');
+};
+
+const askQuestions = (questionGenerator, responseGenerator, questionCount) => {
+  for (let i = 0; i < questionCount; i += 1) {
+    const question = questionGenerator();
+    const userAnswer = getAnswerToQuestion(question);
+    const correctAnswer = responseGenerator(question);
+    verdict(correctAnswer, userAnswer);
+  }
+};
+
+const congratulate = () => {
+  console.log(`Congratulations, ${USER_NAME}!`);
+};
+
 export const greet = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  return userName;
+  USER_NAME = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${USER_NAME}!`);
 };
 
 export const playEvenGame = () => {
-  const userName = greet();
-  const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-  console.log(gameRule);
-  const questionCount = 3;
-  for (let i = 0; i < questionCount; i += 1) {
-    const guessedNumber = random(0, 100);
-    console.log(`Question: ${guessedNumber}`);
-    const userAnswer = String(readlineSync.question('Your answer: '));
-    const correctAnswer = isEven(guessedNumber) ? 'yes' : 'no';
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-      console.log(`Let's try again, ${userName}!`);
-
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+  greet();
+  tellRule('Answer "yes" if the number is even, otherwise answer "no".');
+  askQuestions(
+    () => random(0, 100),
+    (question) => (isEven(question) ? 'yes' : 'no'),
+    3,
+  );
+  congratulate();
 };
